63707f9fcc3353c04988e7607b755aefc1f64042
Julien Faeh <julien.fah@epfl.ch>
2026-07-07 13:48:19 +0200

    Allow use of --objective-window to set an objective frozen window, that will be matched by adding s orbitals on the lowest multiplicity Wyckoff position

--- src/pawan/auto_proj_and_windows.py
+++ src/pawan/auto_proj_and_windows.py
@@ -9,19 +9,15 @@ from wannierberri.symmetry.projections import Projection, ProjectionsSet
 from ase.dft.bandgap import bandgap
 from gpaw.mpi import serial_comm,world
 
-def get_proj_set(K=1.2,seed=None,dir="test",dos_kwargs={'spin': 0, 'npts': 1001, 'width': 0.05},gap_thres=0.1,maximize_fw=False,comm=serial_comm):
+from pawan.extend_proj_set import extend_to_energy_window
+from pawan.utils import safety_check_windows
+
+def get_proj_set(K=1.2,seed=None,dir="test",dos_kwargs={'spin': 0, 'npts': 1001, 'width': 0.05},gap_thres=0.1,maximize_fw=False,objective_wd=None,comm=serial_comm):
     calc = GPAW(f'{dir}/{seed}/{seed}-nscf-irred.gpw', txt=None, communicator=comm)
-    selected_orbitals, outer_win, frozen_win,nwann = Zhang_projection_method(K=K,dir=dir,seed=seed,calc=calc,dos_kwargs=dos_kwargs,gap_thres=gap_thres,maximize_fw=maximize_fw,comm=comm)
+    selected_orbitals, outer_win, frozen_win,nwann = Zhang_projection_method(K=K,dir=dir,seed=seed,calc=calc,dos_kwargs=dos_kwargs,gap_thres=gap_thres,maximize_fw=maximize_fw,objective_wd=objective_wd,comm=comm)
 
     space_group = SpaceGroup.from_gpaw(calc)
 
-    #log the selected orbitals and the windows
-    log_file_path = Path_(f"{dir}/{seed}/orbitals_and_windows.txt")
-    with open(log_file_path, "w") as f:
-        f.write(f"Selected orbitals (atom symbol, atom index, orbital): {[(calc.atoms[i].symbol,i,l) for i,(n,l) in selected_orbitals]},\n")
-        f.write(f"Outer window: {outer_win}\n")
-        f.write(f"Frozen window: {frozen_win}\n")
-        f.write(f"Number of Wannier functions: {nwann}\n")
         
     projs = []
     # group atoms by species
@@ -46,11 +42,25 @@ def get_proj_set(K=1.2,seed=None,dir="test",dos_kwargs={'spin': 0, 'npts': 1001,
                 rotate_basis=True
             )
             projs.append(proj)
+    proj_set = ProjectionsSet(projections=projs)
+    if objective_wd is not None and frozen_win[1] < objective_wd[1]:
+        energies, dos_total = calc.get_dos(**dos_kwargs)
+        print(f"Warning: The frozen window was capped to {frozen_win[1]} eV, which is below the objective frozen window of {objective_wd[1]} eV. Adding s orbitals from the next smallest-multiplicity Wyckoff position to extend the projection set.")
+        proj_set, outer_win, frozen_win,nwann = extend_to_energy_window(calc,energies,dos_total,K,nwann, proj_set, outer_win, frozen_win, objective_wd)
+    
+    #log the selected orbitals and the windows
+    log_file_path = Path_(f"{dir}/{seed}/orbitals_and_windows.txt")
+    with open(log_file_path, "w") as f:
+        f.write(f"Selected orbitals (atom symbol, atom index, orbital): {[(calc.atoms[i].symbol,i,l) for i,(n,l) in selected_orbitals]},\n")
+        f.write(f"Outer window: {outer_win}\n")
+        f.write(f"Frozen window: {frozen_win}\n")
+        f.write(f"Number of Wannier functions: {nwann}\n")
     with open(log_file_path, "a") as f:
-        f.write(f"Selected Projections:\n {'\n'.join(map(str, projs))},\n")
-    return ProjectionsSet(projections=projs), outer_win, frozen_win, nwann
+        f.write(f"Selected Projections (+ non-atomic-centered ones):\n {'\n'.join(map(str, proj_set.projections))},\n")
 
-def Zhang_projection_method(K=1.2, dir="test", seed=None, calc=None, dos_kwargs={'spin': 0, 'npts': 1001, 'width': 0.05},gap_thres=0.1, maximize_fw=False, comm=serial_comm):
+    return proj_set, outer_win, frozen_win, nwann
+
+def Zhang_projection_method(K=1.2, dir="test", seed=None, calc=None, dos_kwargs={'spin': 0, 'npts': 1001, 'width': 0.05},gap_thres=0.1, maximize_fw=False, objective_wd=None, comm=serial_comm):
     '''Placeholder for Zhang's projection method, which will be implemented in the future.'''
     if calc is None:
         calc = GPAW(f'{dir}/{seed}/{seed}-nscf-irred.gpw', txt=None, communicator=comm)
@@ -63,6 +73,8 @@ def Zhang_projection_method(K=1.2, dir="test", seed=None, calc=None, dos_kwargs=
     alpha_max    = {l:  2*l_num[l]+1      for l in l_conversion.values()}  # 100% threshold
 
     Emin_0, Emax_0,pdos,candidates = initial_DOS_energy_scan(calc=calc, dir=dir, seed=seed, dos_kwargs=dos_kwargs, gap_thres=gap_thres) 
+    if objective_wd is not None:
+        Emin_0, Emax_0 = min(objective_wd[0], Emin_0), max(objective_wd[1], Emax_0)  # override with user-defined window
     print(f"Initial outer window: {Emin_0} to {Emax_0} eV")
     print(f"Candidates for projections: {candidates}")
     # then integrate each orbital's pDOS and compare with occupation tolerance alpha
@@ -113,41 +125,17 @@ def Zhang_projection_method(K=1.2, dir="test", seed=None, calc=None, dos_kwargs=
     #refine the orbital selection based on the refined outer window
     selected_orbitals, nwann = alpha_selection(alpha, Emin_0, emax_refined)
     print(f"Refined projections: {selected_orbitals}, nwann={nwann}")
-    '''# ensure that at least nwann bands are inside the window for any k point
-    eigs = np.array([calc.get_eigenvalues(kpt=k) for k in range(len(calc.get_ibz_k_points()))])
-    for k_eigs in eigs:
-        in_window = k_eigs[(k_eigs >= Emin_0) & (k_eigs <= emax_refined)]
-        if len(in_window) < nwann:
-            above_emin = k_eigs[k_eigs >= Emin_0]
-            emax_refined = max(emax_refined, above_emin[nwann-1])  # set emax to include at least nwann bands
-            print(f"Adjusted emax to {emax_refined} eV to include at least {nwann} bands at any k-point.")
 
-    # ensure that there is no more than nwann bands in the frozen window for any k point
-    froz_max = e_fermi + 2  # start with a guess
-    #froz_max = calc.get_homo_lumo()[1]+2  # bottom of conduction band if there is one
-    for k_eigs in eigs:
-        in_outer = k_eigs[(k_eigs >= Emin_0) & (k_eigs <= emax_refined)]
-        in_frozen = in_outer[(in_outer >= Emin_0) & (in_outer <= froz_max)]
-        if len(in_frozen) > nwann:
-            froz_max = min(froz_max, in_frozen[nwann] - 0.01)
-            print(f"Frozen window capped to {froz_max:.3f} eV (nfrozen must be < nwann={nwann})")
-
-    # check if there is at least one band btw froz_max and emax_refined at any k point
-    for k_eigs in eigs:
-        in_free = k_eigs[(k_eigs > froz_max) & (k_eigs <= emax_refined)]
-        if len(in_free) == 0:
-            above_frozen = k_eigs[k_eigs > froz_max]
-            if len(above_frozen) > 0:
-                emax_refined = max(emax_refined, above_frozen[0] + 0.01)
-                print(f"Extended emax to {emax_refined:.3f} eV to ensure free bands exist at all k-points.")
-    
-    out_win = (Emin_0, emax_refined)
-    frozen_win = (Emin_0, froz_max)'''
     e_froz_max_0 = e_fermi + 2
+    e_froz_min_0 = Emin_0
+
     if maximize_fw:
         e_froz_max_0 = emax_refined  # ensure frozen window is below outer window
-    print(f"before safety check: Outer window: {Emin_0} to {emax_refined} eV, Frozen window: {Emin_0} to {e_froz_max_0} eV")
-    out_win, frozen_win = safety_check_windows(calc,nwann,(Emin_0, emax_refined),(Emin_0, e_froz_max_0))
+    if objective_wd is not None:
+        e_froz_max_0 = objective_wd[1]  # use objective frozen window if provided
+        e_froz_min_0 = objective_wd[0]
+    print(f"before safety check: Outer window: {Emin_0} to {emax_refined} eV, Frozen window: {e_froz_min_0} to {e_froz_max_0} eV")
+    out_win, frozen_win = safety_check_windows(calc,nwann,(Emin_0, emax_refined),(e_froz_min_0, e_froz_max_0))
     #could do all checks in one kpoints loop, more efficient, but this is clearer for now
 
     print(f"Selected orbitals: {selected_orbitals}")
@@ -156,37 +144,6 @@ def Zhang_projection_method(K=1.2, dir="test", seed=None, calc=None, dos_kwargs=
 
     return selected_orbitals, out_win, frozen_win, nwann
 
-def safety_check_windows(calc,nwann,outer_win, frozen_win):
-    emin_0, emax_0 = outer_win
-    emax_refined = emax_0
-    # ensure that at least nwann bands are inside the window for any k point
-    eigs = np.array([calc.get_eigenvalues(kpt=k) for k in range(len(calc.get_ibz_k_points()))])
-    for k_eigs in eigs:
-        in_window = k_eigs[(k_eigs >= emin_0) & (k_eigs <= emax_0)]
-        if len(in_window) < nwann:
-            above_emin = k_eigs[k_eigs >= emin_0]
-            emax_refined = max(emax_refined, above_emin[nwann-1])  # set emax to include at least nwann bands
-            print(f"Adjusted emax to {emax_refined} eV to include at least {nwann} bands at any k-point.")
-
-    # ensure that there is no more than nwann bands in the frozen window for any k point
-    froz_max = frozen_win[1]  # start with the provided frozen window max
-    #froz_max = calc.get_homo_lumo()[1]+2  # bottom of conduction band if there is one
-    for k_eigs in eigs:
-        in_outer = k_eigs[(k_eigs >= emin_0) & (k_eigs <= emax_refined)]
-        in_frozen = in_outer[(in_outer >= emin_0) & (in_outer <= froz_max)]
-        if len(in_frozen) > nwann:
-            froz_max = min(froz_max, in_frozen[nwann] - 0.01)
-            print(f"Frozen window capped to {froz_max:.3f} eV (nfrozen must be < nwann={nwann})")
-
-    # check if there is at least one band btw froz_max and emax_refined at any k point
-    for k_eigs in eigs:
-        in_free = k_eigs[(k_eigs > froz_max) & (k_eigs <= emax_refined)]
-        if len(in_free) == 0:
-            above_frozen = k_eigs[k_eigs > froz_max]
-            if len(above_frozen) > 0:
-                emax_refined = max(emax_refined, above_frozen[0] + 0.01)
-                print(f"Extended emax to {emax_refined:.3f} eV to ensure free bands exist at all k-points.")
-    return (emin_0, emax_refined),(emin_0, froz_max)
 
 def initial_DOS_energy_scan(calc=None, dir="test", seed=None, dos_kwargs={'spin': 0, 'npts': 1001, 'width': 0.05}, gap_thres=0.1, comm=serial_comm):
     if calc is None:
@@ -207,14 +164,7 @@ def initial_DOS_energy_scan(calc=None, dir="test", seed=None, dos_kwargs={'spin'
 
     # identify candidates (all valence shells + first empty shells)
     candidates = []
-    '''for iatom, atom in enumerate(atoms):
-        setup = setups[iatom]
-        occupied_ls = set(
-            (n,l_conversion[l]) for n, l, f in zip(setup.n_j, setup.l_j, setup.f_j) if f > 0
-        )
-        for nl_str in occupied_ls:
-            candidates.append((iatom, nl_str))#(iatom, (n, l_str)))
-    '''
+
     candidates = [(iatom, (4,l)) for l in ['s', 'p', 'd'] for iatom, atom in enumerate(atoms)]
 
     # compute orbital PDOS for each candidate
@@ -243,25 +193,6 @@ def initial_DOS_energy_scan(calc=None, dir="test", seed=None, dos_kwargs={'spin'
     ef_idx = np.searchsorted(energies, e_fermi)
 
     for (iatom, l), dos in pdos.items():
-        '''# scan downward from E_F to find VBM for this orbital
-        below = dos[:ef_idx][::-1]
-        nonzero_below = np.where(below > threshold)[0]
-        if len(nonzero_below) == 0:
-            continue
-        # start scanning from the top of the valence band, not from E_F
-        vbm_idx = ef_idx - nonzero_below[0]
-
-        # now scan downward from VBM to find where DOS goes to zero
-        below_vbm = dos[:vbm_idx][::-1]
-        zero_below = np.where(below_vbm < threshold)[0]
-        emin_ij = energies[vbm_idx - zero_below[0]] if len(zero_below) > 0 else energies[0]
-
-        # scan upward from E_F
-        above = dos[ef_idx:]
-        zero_above = np.where(above < threshold)[0]
-        emax_ij = energies[ef_idx + zero_above[0]] if len(zero_above) > 0 else energies[-1] #correct??
-        emin_list.append(emin_ij)
-        emax_list.append(emax_ij)'''  
         emin_ij, emax_ij = find_zero_dos_window(energies, dos, e_fermi, gap=gap, threshold=1e-10, gap_thres=gap_thres)
         emin_list.append(emin_ij)
         emax_list.append(emax_ij)
